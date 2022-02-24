bbe5b3d6af85dea11997cc4e3afb114a347b9b7e
Andrew Parsons <atparsons@gmail.com>
2022-02-24 10:14:07 -0500

    added state dependency, simple parser and command interface

--- ad_hoc/my_classes.py
+++ ad_hoc/my_classes.py
@@ -1,11 +1,11 @@
 class World():
-    def __init__(self, name, rooms=[]):
+    def __init__(self, name: str, rooms:list = []) -> None:
         self.name = name
         self.rooms = rooms
 
-    def __str__(self):
+    def __str__(self) -> str:
         result = f"{self.name}\n=====\nNumber of rooms: {len(self.rooms)}\n=====\n"
-        for r in self.rooms:
+        for r in self.rooms.values():
             result += str(r) + '\n'
             for c in r.get_characters_in_room():
                 result += str(c) + '\n'
@@ -13,66 +13,97 @@ class World():
                 result += str(i) + '\n'
         return result
 
-    def add_rooms(self, rooms):
-        self.rooms += list(rooms)
+    def add_rooms(self, rooms:dict) -> None:
+        self.rooms.update(rooms)
 
-    def get_rooms(self):
+    def get_rooms(self) -> set:
         return self.rooms
 
 
 class Room:
-    def __init__(self, name, state, state_id):
+    def __init__(self, name: str, state: dict, state_id: str) -> None:
         self.name = name
         self.state = state
         self.state_id = state_id
         self.description = state[state_id]['description']
-        self.connections = set({})
-        self.characters_in_room = set({})
-        self.items_in_room = set({})
+        self.connections = {}
+        self.characters_in_room = {}
+        self.items_in_room = {}
 
-    def __str__(self):
+    def __str__(self) -> str:
         return(f"{self.name}\n=====\nDescription: {self.description}\n=====\nNum Connections: {len(self.connections)}\nNum Characters: {len(self.characters_in_room)}\n")
 
-    def add_connection(self, connection):
-        self.connections.add(connection)
+    def add_connection(self, direction: str, connection) -> None:
+        self.connections.update({direction:connection})
 
-    def add_character(self, character):
-        self.characters_in_room.add(character)
+    def add_character(self, character) -> None:
+        self.characters_in_room.update({character.get_name():character})
 
-    def add_item(self, item):
-        self.items_in_room.add(item)
+    def add_item(self, item) -> None:
+        self.items_in_room.update({item.get_name():item})
 
-    def get_connections(self):
+    def get_connections(self) -> dict:
         return self.connections
 
-    def get_characters_in_room(self):
+    def get_characters_in_room(self) -> dict:
         return self.characters_in_room
 
-    def get_items_in_room(self):
+    def get_items_in_room(self) -> dict:
         return self.items_in_room
+
+    def get_description(self) -> str:
+        return self.description
+
+    def remove_connection(self,connection_name:str) -> None:
+        self.connections.pop(connection_name)
+
+    def remove_character(self,character) -> None:
+        self.characters_in_room.pop(character.get_name())
+
+    def remove_item(self,item) -> None:
+        self.items_in_room.pop(item.get_name())
 
 
 class Character():
-    def __init__(self, name, state, state_id, starting_loc, is_playable):
+    def __init__(self, name:str, state:dict, state_id:str, current_loc:str, is_playable:bool) -> None:
         self.name = name
         self.state = state
         self.state_id = state_id
         self.description = state[state_id]['description']
-        self.starting_loc = starting_loc
+        self.current_loc = current_loc
         self.is_playable = is_playable
+        self.inventory = {}
  
-    def __str__(self):
+    def __str__(self) -> str:
         return(f"{self.name}\n=====\nDescription: {self.description}\nIs Playable? {self.is_playable}\n")
+
+    def set_location(self, new_loc: str) -> None:
+        self.current_loc = new_loc
+
+    def get_location(self) -> str:
+        return self.current_loc
+
+    def add_to_inventory(self, item) -> None:
+        self.inventory.update({item.name:item})
+
+    def is_player(self) -> bool:
+        return self.is_playable
+
+    def get_name(self) -> str:
+        return self.name
 
 
 class Item():
-    def __init__(self, name, state, state_id, starting_loc, can_be_picked_up):
+    def __init__(self, name:str, state:dict, state_id:str, current_loc:str, can_be_picked_up:bool) -> None:
         self.name = name
         self.state = state
         self.state_id = state_id
         self.description = state[state_id]['description']
-        self.starting_loc = starting_loc
+        self.current_loc = current_loc
         self.can_be_picked_up = can_be_picked_up
  
-    def __str__(self):
+    def __str__(self) -> str:
         return(f"{self.name}\n=====\nDescription: {self.description}\n")
+
+    def get_name(self) -> str:
+        return self.name
--- ad_hoc/test_generate_world.py
+++ ad_hoc/test_generate_world.py
@@ -1,45 +1,160 @@
+###############################################################
+#################### Import Relevant Files ####################
+###############################################################
+
 from my_classes import World, Room, Item, Character
 import json
 
+###############################################################
+##################### Set Global Variables ####################
+###############################################################
+
+assets_file_name = 'assets.json'
+commands_file_name = 'commands.json'
+ignore_tokens = ['on','at','from','to','the']
+
 # load game data from file to dictionary
-with open('assets.json') as json_file:
+with open(assets_file_name) as json_file:
     data = json.load(json_file)
+
+# load acceptable commands from file to dictionary
+with open(commands_file_name) as json_file:
+    commands_dict = json.load(json_file)
+
+###############################################################
+#################### World Building Section ###################
+###############################################################
 
 # create characters
 if 'characters' in data.keys():
-	characters = [Character(x['name'],x['state'],"0",x['starting_loc'],x['is_playable']) 
-				  for x in data['characters']]
+	characters = [Character(x['name'],x['state'],"0",x['current_loc'],x['is_playable']) for x in data['characters']]
+	player = [x for x in characters if x.is_player()][0]
 
 # create items
 if 'items' in data.keys():
-	items = [Item(x['name'],x['state'],"0",x['starting_loc'],x['can_be_picked_up']) 
-				  for x in data['items']]
+	items = [Item(x['name'],x['state'],"0",x['current_loc'],x['can_be_picked_up']) for x in data['items']]
 
 # create rooms
 if 'rooms' in data.keys():
-	rooms = []
+	rooms = {}
 	connections = {}
 	for x in data['rooms']:
-		r = Room(x['name'],x['state'],"0")
-		rooms += [r]
+		rooms[x['name']] = Room(x['name'],x['state'],"0")
 		connections[x['name']] = x['connections']
 
 # add connections, items and characters to rooms
-for room in rooms:
+for room in rooms.values():
 	# connect rooms
-	for connection in connections[room.name]:
-		room.add_connection(connection)
+	for direction, connection in connections[room.name].items():
+		room.add_connection(direction,connection)
 	# add characters and items to rooms
 	for character in characters:
-		if room.name == character.starting_loc:
+		if room.name == character.current_loc:
 			room.add_character(character)
 	# add items to rooms
 	for item in items:
-		if room.name == item.starting_loc:
+		if room.name == item.current_loc:
 			room.add_item(item)
 
 # generate world and add rooms to it
 my_world = World(name='my_world', rooms=rooms)
 
 # print summary of world, descriptions of each room, character and item
-print(my_world)
+# print(my_world)
+
+###############################################################
+####################### Helper Functions ######################
+###############################################################
+
+def parse_input(input_text:str) -> dict:
+	split_text = [str.lower(x) for x in input_text.split()]
+	result = {}
+	while len(split_text) > 0:
+		if 'action' not in result.keys():
+			result, split_text = get_action_command(result, split_text)
+		elif 'target1' not in result.keys():
+			result, split_text = get_target_command(result, split_text, 1)
+		elif 'target2' not in result.keys():
+			result, split_text = get_target_command(result, split_text, 2)
+		else:
+			split_text.pop(0)
+	return result
+
+def get_action_command(result:dict, split_text:list) -> tuple[dict, list]:
+	while 'action' not in result.keys():
+		if len(split_text) == 0:
+			print("ERROR - action command not found.")
+			break
+		token = split_text[0]
+		if token in commands_dict['actions'].keys():
+			result.update(commands_dict['actions'][token])
+			split_text.pop(0)
+		elif token in ignore_tokens:
+			split_text.pop(0)
+		else:
+			if len(split_text) > 1:
+				split_text[0] += split_text[1]
+			else:
+				split_text.pop(0)
+	return result, split_text
+
+def get_target_command(result:dict, split_text:list,target_num:int) -> tuple[dict, list]:
+	while 'target' not in result.keys():
+		if len(split_text) == 0:
+			print(f"ERROR - target {target_num} not found.")
+			break
+		token = split_text[0]
+		if token in commands_dict['targets'].keys():
+			target_result = commands_dict['targets'][token]
+			target_result['target'+str(target_num)] = target_result.pop('target')
+			result.update(target_result)
+			split_text.pop(0)
+		elif token in ignore_tokens:
+			split_text.pop(0)
+		else:
+			if len(split_text) > 1:
+				split_text[0] += split_text[1]
+			else:
+				split_text.pop(0)
+
+	return result, remaining_text
+
+def process_command(command:dict) -> None:
+	# takes in a dictionary of {'action': x, 'target1':y, ['target2':z]}
+	# this function should choose the appropriate objects to action and if needed, update state
+	if 'action' not in command.keys():
+		print("Did not understand command. Say 'help' for list of command words.")
+		return None
+	print(command)
+
+	if command['action'] in ['help']:
+		print("List of possible commands:\n")
+		for k in commands_dict['actions'].keys():
+			print(k)
+
+	if command['action'] in ['describe']:
+		print(my_world.get_rooms()[player.get_location()].get_description())
+
+	if command['action'] in ['move']:
+		# identify room player current in, check connections dict to see if target1 has a connection available in the direction
+		current_room = player.get_location()
+		next_room = my_world.get_rooms()[current_room].get_connections().get(command['target1'],None)
+
+		if next_room is not None:
+			# if so, remove player from current room, add them to new room, and update the current_loc for the player
+			player.set_location(next_room)
+			my_world.get_rooms()[current_room].remove_character(player)
+			my_world.get_rooms()[next_room].add_character(player)
+	return None
+
+###############################################################
+##################### Interactive Section #####################
+###############################################################
+
+# accept commands in a permenent while loop
+cmd_input=''
+while(True):
+	cmd_input = input("\nprompt: ")
+	if str.lower(cmd_input) == 'q':
+		break
+	process_command(parse_input(cmd_input))
