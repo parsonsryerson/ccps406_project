405935b4fbebf04fdb8b38c77a1d99296c8a1889
Andrew Parsons <atparsons@gmail.com>
2022-03-31 16:15:45 -0400

    progress update

--- game_classes_old.py
+++ game_classes_old.py
@@ -8,18 +8,132 @@ class CommandParser():
         self._target_name = None
         self._ignore_tokens = ['on','at','from','to','the']
 
+    def try_parse(self, command:str) -> str:
+        parse_result = self.parse_command(command)
+        if parse_result == "success":
+            # get required parameters to create an Action
+            if self._action_name is not None and self._target_name is not None:
+                action_params = self._world.get_available_actions().get(self._action_name).get(self._target_name)
+                print(f"in try_parse -- action_params: {action_params}")
+                action = Action(
+                    name = self._action_name,
+                    actor_name = self._actor_name, 
+                    target_name = self._target_name, 
+                    req_state = action_params.get('req_state',None), 
+                    next_state = action_params.get('next_state',None), 
+                    description_success = action_params.get('description_success',None)
+                )
+                action_result = action.perform_action(self._world)
+            else:
+                action_result = 'error - target'
+        else:
+            action_result = parse_result
+        print(f"in try_parse -- action_name: {self._action_name}, target_name: {self._target_name}, action_result: {action_result}")
+        return action_result
+
+
     # helper function
     def flatten(self,t):
         return [item for sublist in t for item in sublist]
 
-    def try_parse(self, command:str) -> str:
+    # Translates input string into three key aspects
+    # - Actor - the object performing the action
+    # - Action - the action being performed
+    # - Target - the object the action is being performed on
+    # Returns the phrase to send to the view. 
+    # - If successful, this is an action desription
+    # - If one of action or target can't be found, then sends appropriate error message.
+    def parse_command(self, command: str) -> str:
+        result = ''
+        ## parse the command
         split_text = self.flatten([self._world.get_available_commands().get(str.lower(x),'').split() for x in command.split()])
-        print(f"command: {command}, split_text: {split_text}")
-        assert(len(split_text) >= 1 and len(split_text) <= 2)
-        self._action_name = split_text[0]
-        self._target_name = split_text[1] if len(split_text) > 1 else None
-        action = Action(self._world, self._action_name, self._actor_name, self._target_name).perform_action()
+        print(f"in parse_command -- command: {command}, split_text: {split_text}")
+        while len(split_text) > 0:
+            if self._action_name is None:
+                action_name, split_text = self.__find_action_name(split_text)
+                print(f"in parse_command -- action_name: {action_name}, split_text: {split_text}")
+                if action_name in ('help','describe'):
+                    result = action_name
+                    break
+                elif action_name is not None:
+                    self.__set_action_name(action_name)
+                    if len(split_text) == 0:
+                        result = "success"
+                        break
+                else:
+                    result = "error action"
+                    break
+            elif self._target_name is None:
+                target_name, split_text = self.__find_target_name(split_text)
+                print(f"in parse_command -- target_name: {target_name}, split_text: {split_text}")
+                if target_name is not None:
+                    self.__set_target_name(target_name)
+                    if len(split_text) == 0:
+                        result = "success"
+                        break
+                elif self._action_name == 'describe':
+                    self._target_name = self._world.get_characters().get(self._world.get_player_name()).get_room_name()
+                    if len(split_text) == 0:
+                        result = "success"
+                        break
+                elif self._action_name == 'move':
+                    result = 'error move'
+                    break
+                else:
+                    result = "error target"
+                    break
+            else:
+                result = "success"
         return result
+
+    def __find_action_name(self, split_text:[]):
+        while(self._action_name is None):
+            if len(split_text) == 0:
+                break
+            token = split_text[0]
+            print(f"in __find_action_name -- token: {token}, available_actions: {self._world.get_available_actions().keys()}")
+            if token in self._world.get_available_actions().keys():
+                split_text.pop(0)
+                return token, split_text
+            elif token in self._ignore_tokens:
+                split_text.pop(0)
+            else:
+                if len(split_text) > 1:
+                    split_text[0] += split_text[1]
+                else:
+                    split_text.pop(0)
+        return None, split_text
+
+    def __find_target_name(self, split_text:[]):
+        current_room_name = self._world.get_characters().get(self._world.get_player_name()).get_room_name()
+        print(f"in __find_target_name -- \ncharacters: {self._world.get_characters().keys()}, current_room_name: {current_room_name}")
+        print(f"in __find_target_name -- \nconnections: {self._world.get_rooms().get(current_room_name).get_connections().keys()}")
+        while(self._target_name is None):
+            if len(split_text) == 0:
+                break
+            token = split_text[0]
+            print(f"in __find_target_name -- token: {token},\ncharacters: {self._world.get_characters().keys()},\nitems: {self._world.get_items().keys()},\nrooms: {self._world.get_rooms().get(current_room_name).get_connections().keys()}")
+            if token in self._world.get_characters().keys():
+                print(f"found in characters\n")
+                split_text.pop(0)
+                return token, split_text
+            elif token in self._world.get_items().keys():
+                print(f"found in items\n")
+                split_text.pop(0)
+                return token, split_text
+            elif token in self._world.get_rooms().get(current_room_name).get_connections().keys():
+                print(f"found in rooms\n")
+                split_text.pop(0)
+                return token, split_text
+            elif token in self._ignore_tokens:
+                print(f"found in ignore_tokens\n")
+                split_text.pop(0)
+            else:
+                if len(split_text) > 1:
+                    split_text[0] += split_text[1]
+                else:
+                    split_text.pop(0)
+        return None, split_text
 
     # Setter Methods (private)
     def __set_action_name(self, action_name: str) -> None:
@@ -159,8 +273,7 @@ class WorldObject():
         return self._name
 
     def get_description(self) -> str:
-        print(f"DEBUG - in WorldObject - state: {self._state}, current_state: {self._current_state}")
-        result = self._state.get(self._current_state).get('description','').get(_description_length,'')
+        result = _state.get(self._current_state,'').get('description','').get(_description_length,'')
         # Set description length to 'short' when 'long' description is requested
         if self._description_length == 'long':
             self._description_length = 'short'
@@ -193,9 +306,6 @@ class Room(WorldObject):
     def set_game_objects(self, game_objects:dict) -> None:
         self._game_objects.update(game_objects)
 
-    def remove_game_objects(self, game_object_key:str) -> None:
-        self._game_objects.pop(game_object_key)
-
     # Getter Methods
     def get_connections(self) -> dict:
         return self._connections
@@ -214,6 +324,16 @@ class GameObject(WorldObject):
     def __init__(self, name:str, state:dict, starting_room_name:str) -> None:
         super().__init__(name,state)
         self._current_room_name = starting_room_name
+
+    # # Performs the action by the actor on the target
+    # def perform_action(action:str, world) -> None:
+    #     # tbd
+    #     # if 'action' is performed: 
+    #     # check data for what the state transition looks like for
+    #     # both the game object and the target 
+    #     # should be list of "available actions" for each object in each state
+    #     # it should contain the next state name for both actor and target
+    #     pass
 
     # Setter Method
     def set_room_name(self,room_name) -> None:
@@ -285,72 +405,64 @@ class Item(GameObject):
 class Action():
 
     # Constructor
-    def __init__(self, world, name:str, actor_name:str, target_name:str) -> None:
-        self._world = world
-        self._action_name = name
+    def __init__(self, name:str, actor_name:str, target_name:str, req_state:dict, next_state:dict, description_success:str) -> None:
+        self._name = name
         self._actor_name = actor_name
         self._target_name = target_name
-        self._req_state = self._world.get_available_actions().get(self._action_name).get('req_state')
-        self._next_state = self._world.get_available_actions().get(self._action_name).get('next_state')
-        self._description_success = self._world.get_available_actions().get(self._action_name).get('description_success')
+        self._req_state = req_state
+        self._next_state = next_state
+        self._description_success = description_success
 
-    def perform_action(self) -> str:
+    def perform_action(self, world) -> str:
         # initialize result to empty string
         result = ''
 
-        # special case - "help" action simply returns help - no changes to game world or state
-        if self._action_name == "help":
-            result = self._name
+        print(f"in perform_action -- name: {self._name}")
         # special case - "describe" action always meets conditions
-        elif self._action_name == "describe":
-            result = self.__perform_describe()
-        # special case - "move" action requires different conditions
-        elif self._action_name == "move":
-            result = self.__perform_move()
+        if self._name == "describe":
+            result = self.__perform_describe(world)
 
         # general case - check if conditions for actions are met, if so update states and return success description
-        elif self.__meets_conditions():
-            self.__update_states()
+        if self.__meets_conditions(world):
+            print(f"in perform_action (meets conditions)")
+            self.__update_states(world)
             result = self._description_success
+            print(f"in perform_action (description success) -- result: {result}")
         return result
 
-    def __perform_describe(self) -> str:
+    def __perform_describe(self, world) -> str:
         # if no target specified, assume target is current room of player
         if self._target_name is None:
-            actor = self._world.get_world_objects().get(self._world.get_player_name(),None)
-            target = self._world.get_world_objects().get(actor.get_room_name(),None)
+            actor = world.get_world_objects().get(world.get_player_name(),None)
+            target = world.get_world_objects().get(actor.get_room_name(),None)
         else:
-            target = self._world.get_world_objects().get(self._target_name,None)
+            target = world.get_world_objects().get(self._target_name,None)
         return target.get_description()
 
-    def __perform_move(self) -> str:
-        # translate the name of the target into the correct room name based on the actor's current location
-        prev_room_name = self._world.get_characters().get(self._actor_name).get_room_name()
-        next_room_name = self._world.get_rooms().get(prev_room_name).get_connections().get(self._target_name)
-        # update the actor's location to the next room
-        self._world.get_characters().get(self._actor_name).set_room_name(self._target_name)
-        # update the previous and next room's game objects
-        self._world.get_rooms().get(prev_room_name).remove_game_objects(self._actor_name)
-        self._world.get_rooms().get(next_room_name).set_game_objects({self._actor_name:self._world.get_characters().get(self._actor_name)})
-        # return description of next room
-        return self._world.get_rooms().get(next_room_name).get_description()
-
-    def __update_states(self) -> None:
+    def __update_states(self, world) -> None:
         for name, next_state in self._next_state.items(): 
-            world_object = self._world.get_world_objects().get(name,None)
+            world_object = world.get_world_objects().get(name,None)
             if world_object is not None:
                 world_object.set_current_state(next_state)
 
-    def __meets_conditions(self) -> bool:
+    def __meets_conditions(self, world) -> bool:
         # initialize result to False
         result = False
 
         # get references to actor and target from the world object
         # actor = world.get_world_objects().get(self._actor_name,None)
-        actor = self._world.get_characters().get(self._world.get_player_name())
-        for world_object_type, world_object in self._world.get_world_objects().items():
+        actor = world.get_characters().get(world.get_player_name())
+        for world_object_type, world_object in world.get_world_objects().items():
+            print(f"world_object_type: {world_object_type}")
+            print(f"target_name: {self._target_name}")
+            print(f"world_object.keys(): {world_object.keys()}")
             if self._target_name in world_object.keys():
+                print(f"found target name: {self._target_name}")
                 target = world_object.get(self._target_name,None)
+        
+        # print(f"in __meets_conditions -- world_objects: {world.get_world_objects()}")
+        print(f"in __meets_conditions -- actor_name: {self._actor_name}, target_name: {self._target_name}")
+        print(f"in __meets_conditions -- actor: {actor}, target: {target}")
 
         # if either of the actor or target is not specified then return False
         if actor is None or target is None:
